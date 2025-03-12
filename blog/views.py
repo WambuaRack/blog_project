0fd514d34fb7effef501c3fdd747f1f0e293c99c
Wambuarack <shedrackwambua40@gmail.com>
2025-03-12 14:42:07 +0300

    complete

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render,get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse 
-from . models import post 
+from django.urls import reverse
+from .models import post  # ✅ Use uppercase 'Post' to match the model name
 
 def post_list(request):
-    posts=post.objects.all().order_by('date_posted')
-    return render(request,'blog/post_list.html',{'post':post})
+    posts = post.objects.all().order_by('-date_posted')  # ✅ Changed 'post' to 'Post'
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, post_id):
 
-def post_detail(request):
-    post=get_list_or_404(post,post_id)
-    return render(request,'blog/post_list.html',{'post':post})
+  
+    return render(request, "blog/post_detail.html", {"post": post})
